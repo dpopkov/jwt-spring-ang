96bea44b8896020e69f20d38dda66a4151dd2203
Denis Popkov <pkvdenis@gmail.com>
2022-04-27 16:18:43 +0300

    Add Spring Boot project.

---
+++ src/main/java/learn/springang/umsportal/UmsPortalApplication.java
@@ -0,0 +1,13 @@
+package learn.springang.umsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmsPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/springang/umsportal/UmsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.springang.umsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
